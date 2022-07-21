a79850ca7b49247daa0096fbc73dd180876ec00f
santu <santu@DESKTOP-CU5DURS>
2022-07-21 12:35:13 +0530

    first commit

---
+++ com.Tyss/src/test/java/com/Tyss/MyFirstProjectTest.java
@@ -0,0 +1,16 @@
+package com.Tyss;
+
+import org.testng.annotations.Test;
+
+public class MyFirstProjectTest {
+
+	 
+		@Test
+		public void MyTest()
+		{
+			System.out.println("My first test");
+		}
+
+	}
+
+
